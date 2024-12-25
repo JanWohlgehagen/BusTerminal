2b284cb3578874f6db29daa89b475440e0ca4261
Mikkel Theut <mikkeltheut@gmail.com>
2024-12-25 11:27:17 +0100

    Price impleneted

--- BusTerminalApp/Application/Interfaces/IPriceService.cs
+++ BusTerminalApp/Application/Interfaces/IPriceService.cs
@@ -2,5 +2,12 @@
 
 public interface IPriceService
 {
+    /// <summary>
+    /// Calculates the price of a busride. use Datetime.DayOfWeek and distance must be greater than 0 and in whole kilometers.
+    /// </summary>
+    /// <param name="bookingDay"></param>
+    /// <param name="distance"></param>
+    /// <returns></returns>
+    public decimal GetPrice(DayOfWeek bookingDay, int distance);
     
 }
--- BusTerminalApp/Application/Services/PriceService.cs
+++ BusTerminalApp/Application/Services/PriceService.cs
@@ -2,7 +2,27 @@
 
 namespace Application.Services;
 
-public class PriceService: IPriceService
+public class PriceService : IPriceService
 {
-    
+    public decimal GetPrice(DayOfWeek bookingDay, int distance)
+    {
+        if(distance <= 0)
+        {
+            throw new ArgumentException("We need a distance to calculate the price.");
+        }
+        var startfee = 500;
+        decimal distanceRate = 10;
+        if (bookingDay == DayOfWeek.Saturday)
+        {
+            startfee = 750;
+            distanceRate = 12.5m;
+        }
+        else if (bookingDay == DayOfWeek.Sunday)
+        {
+            startfee = 1000;
+            distanceRate = 15;
+        }
+
+        return startfee + (distanceRate * distance);
+    }
 }
