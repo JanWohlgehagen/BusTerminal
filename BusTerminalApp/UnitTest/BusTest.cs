e2e5e9a7de040c895eeadd1ec02dbb0faac82ecc
Theut94 <89963626+Theut94@users.noreply.github.com>
2024-12-30 20:55:51 +0100

    Book Bus test and added regions

--- BusTerminalApp/UnitTest/BusTest.cs
+++ BusTerminalApp/UnitTest/BusTest.cs
@@ -1,12 +1,15 @@
 using Application.Interfaces;
 using Application.Services;
 using Data.Models;
+using Domain.DTOs;
 using Moq;
 
 namespace UnitTest
 {
     public class BusTest
     {
+
+        #region ValidateTime
         [Theory]
         [InlineData(24, 25)]
         public void ValidTimeTestSucces(int start, int end)
@@ -14,7 +17,7 @@ namespace UnitTest
             //Arrange
             var mockpriceService = new Mock<IPriceService>();
             var mockRepo = new Mock<IBusRepository>();
-            IBusService busService = new BusService(mockRepo.Object, mockpriceService.Object );
+            IBusService busService = new BusService(mockRepo.Object, mockpriceService.Object);
             var starttime = DateTime.Today.AddHours(start);
             var endtime = DateTime.Today.AddHours(end);
 
@@ -28,7 +31,7 @@ namespace UnitTest
         [Theory]
         [InlineData(1, 0, "End time cannot be before start time.")]
         [InlineData(0, 24, "Start and end time must be on the same day.")]
-        [InlineData(-1,1, "Start time cannot be in the past.")]
+        [InlineData(-1, 1, "Start time cannot be in the past.")]
         public void ValidTimeTestFail(int start, int end, string failMessage)
         {
             //Arrange
@@ -39,10 +42,12 @@ namespace UnitTest
             var endtime = DateTime.Now.AddHours(end);
 
             //Act & Assert
-            var ex = Assert.Throws<ArgumentException>(() => busService.ValidateTime(starttime,endtime));
+            var ex = Assert.Throws<ArgumentException>(() => busService.ValidateTime(starttime, endtime));
             Assert.Equal(ex.Message, failMessage);
         }
+        #endregion
 
+        #region GetAvailableBus
         [Theory]
         [MemberData(nameof(TestData.OneUnoccupiedBus), MemberType = typeof(TestData))]
         [MemberData(nameof(TestData.OneBusOccupiedInThreeDays), MemberType = typeof(TestData))]
@@ -50,7 +55,7 @@ namespace UnitTest
         {
             var mockpriceService = new Mock<IPriceService>();
             var mockRepo = new Mock<IBusRepository>();
-            mockRepo.Setup( r => r.GetMockData()).Returns(busListData);
+            mockRepo.Setup(r => r.GetMockData()).Returns(busListData);
             IBusService busService = new BusService(mockRepo.Object, mockpriceService.Object);
 
 
@@ -67,6 +72,7 @@ namespace UnitTest
         [MemberData(nameof(TestData.EmptyDbData), MemberType = typeof(TestData))]
         public void GetAvailableBusesEmpty(List<Bus> busListData, int distance, DateTime bookingDate)
         {
+            //Arrange
             var mockpriceService = new Mock<IPriceService>();
             var mockRepo = new Mock<IBusRepository>();
             mockRepo.Setup(r => r.GetMockData()).Returns(busListData);
@@ -79,6 +85,46 @@ namespace UnitTest
             //Assert
             Assert.Empty(busList);
         }
+        #endregion
 
+
+        #region BookBus
+
+        [Theory]
+        [MemberData(nameof(TestData.BookAvailableBusWithOccupiedDates), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.BookAvailableBusWithNoOccupiedDates), MemberType = typeof(TestData))]
+        public void BookBusSuccesful(List<Bus> busListData, string id, DateTime starttime, DateTime endtime)
+        {
+            //Arrange
+            var mockpriceService = new Mock<IPriceService>();
+            var mockRepo = new Mock<IBusRepository>();
+            mockRepo.Setup(r => r.GetMockData()).Returns(busListData);
+            IBusService busService = new BusService(mockRepo.Object, mockpriceService.Object);
+
+            //Act 
+            var bus = busService.BookBus(id, starttime, endtime);
+
+            //Assert
+            Assert.NotNull(bus);
+            Assert.Equal(bus.Id, id);
+            Assert.IsType<BusDTO>(bus);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.BookBusWrongId), MemberType = typeof(TestData))]
+        [MemberData(nameof(TestData.BookBusOnBookedDay), MemberType = typeof(TestData))]
+        public void BookBusFail(List<Bus> busListData, string id, DateTime starttime, DateTime endtime, string errorMessage)
+        {
+            //Arrange
+            var mockpriceService = new Mock<IPriceService>();
+            var mockRepo = new Mock<IBusRepository>();
+            mockRepo.Setup(r => r.GetMockData()).Returns(busListData);
+            IBusService busService = new BusService(mockRepo.Object, mockpriceService.Object);
+
+            //Act & Assert
+            var ex = Assert.Throws<ArgumentException>( () => busService.BookBus(id, starttime, endtime));
+            Assert.Equal(ex.Message, errorMessage);
+            #endregion
+        }
+        }
     }
-}
--- BusTerminalApp/UnitTest/TestData.cs
+++ BusTerminalApp/UnitTest/TestData.cs
@@ -9,6 +9,7 @@ namespace UnitTest
 {
     public static class TestData
     {
+        #region GetAvailableBusData
         public static List<Bus> EmptyBusList
         {
             get
@@ -34,8 +35,8 @@ namespace UnitTest
                             Name = "Big Green"
                         }
                     },
-                    10, 
-                    DateTime.Today.AddDays(1) 
+                    10,
+                    DateTime.Today.AddDays(1)
                 };
             }
         }
@@ -55,7 +56,7 @@ namespace UnitTest
                             Name = "Big Green"
                         }
                     },
-                    10, 
+                    10,
                     DateTime.Today.AddDays(1)
                 };
             }
@@ -110,13 +111,110 @@ namespace UnitTest
                 yield return new object[]
                 {
                     new List<Bus>
-                    {                     
+                    {
                     },
                     10,
                     DateTime.Today.AddDays(1)
                 };
             }
         }
+        #endregion
 
+        #region BookBusData
+        public static IEnumerable<object[]> BookAvailableBusWithOccupiedDates
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new List<Bus>
+                    {
+                        new Bus
+                        {
+                            Id = "1",
+                            BookedTimes = new List<DateTime>(){ DateTime.Today.AddDays(3).AddHours(14)},
+                            Capacity = 42,
+                            Name = "Big Green"
+                        }
+                    },
+                    "1",
+                    DateTime.Today.AddDays(1).AddHours(1),
+                    DateTime.Today.AddDays(1).AddHours(2),
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> BookAvailableBusWithNoOccupiedDates
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new List<Bus>
+                    {
+                        new Bus
+                        {
+                            Id = "1",
+                            BookedTimes = new List<DateTime>(){},
+                            Capacity = 42,
+                            Name = "Big Green"
+                        }
+                    },
+                    "1",
+                    DateTime.Today.AddDays(1).AddHours(1),
+                    DateTime.Today.AddDays(1).AddHours(2),
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> BookBusWrongId
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new List<Bus>
+                    {
+                        new Bus
+                        {
+                            Id = "1",
+                            BookedTimes = new List<DateTime>(){ DateTime.Today.AddDays(3).AddHours(14)},
+                            Capacity = 42,
+                            Name = "Big Green"
+                        }
+                    },
+                    "-1",
+                    DateTime.Today.AddDays(1).AddHours(1),
+                    DateTime.Today.AddDays(1).AddHours(2),
+                    "Bus not found."
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> BookBusOnBookedDay
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new List<Bus>
+                    {
+                        new Bus
+                        {
+                            Id = "1",
+                            BookedTimes = new List<DateTime>(){ DateTime.Today.AddDays(1).AddHours(14)},
+                            Capacity = 42,
+                            Name = "Big Green"
+                        }
+                    },
+                    "1",
+                    DateTime.Today.AddDays(1).AddHours(1),
+                    DateTime.Today.AddDays(1).AddHours(2),
+                    "Bus is already booked at that time."
+                };
+            }
+        }
+
+        #endregion
     }
 }
